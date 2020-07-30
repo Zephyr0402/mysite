55140a712839924738242cd3704a147b645d1d17
ZephyrOne <bjshenshijun@yahoo.com>
2020-07-30 23:20:02 +0800

    Set backend to use frontend build

--- flask-api/flask-api.py
+++ flask-api/flask-api.py
@@ -1,11 +1,15 @@
 import time
 from flask import Flask, request, render_template, send_file
 
-import os
-flask_path = os.getcwd()
-
-app = Flask(__name__, static_url_path='', root_path=flask_path)
+app = Flask(__name__, static_folder='../build', static_url_path='/')
 
 @app.route('/updating')
 def updating():
    return render_template('updating.html');
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=False)
